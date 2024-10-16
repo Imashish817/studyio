7f31b0ed706eb20992f71d7d175ed7d82b579815
Ashish jaiswal <Imashishjaiswal99@gmail.com>
2024-10-17 03:32:03 +0530

    R2:remove dark

--- src/components/elements/AnimatedModalDemo.js
+++ src/components/elements/AnimatedModalDemo.js
@@ -17,11 +17,6 @@ export function AnimatedModalDemo({name}) {
     "https://images.unsplash.com/photo-1554931670-4ebfabf6e7a9?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1546484475-7f7bd55792da?q=80&w=2581&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ];
-  if (name==='')
-  {
-    alert("Enter some valid input");
-    return;
-  }
   return (
     <div className="py-35  flex items-center justify-center mb-4">
       <Modal>
